eb2cda6004feabd6653f6d97a877faea0b0738fb
unknown <amadip272@gmail.com>
2026-07-02 23:48:43 +0100

    changed-package.json and vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: "/E-commerce",
+  base: "/E-commerce-",
   server: {
     port: 3000,
    }
